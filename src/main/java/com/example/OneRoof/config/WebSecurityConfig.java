aac54746d11819c1ad15432ec0cfe783baf987bc
Ethan Philipson <erphilipson@gmail.com>
2017-08-27 10:25:08 -0400

    Add local database

--- src/main/java/com/example/OneRoof/config/WebSecurityConfig.java
+++ src/main/java/com/example/OneRoof/config/WebSecurityConfig.java
@@ -28,7 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/newuser", "/signup").permitAll()
+                    .antMatchers("/", "/newuser", "/signup", "/api/*").permitAll()
                     .antMatchers("/admin").hasRole("ADMIN")
                     .anyRequest().authenticated()
                     .and()
--- src/main/java/com/example/OneRoof/domain/User.java
+++ src/main/java/com/example/OneRoof/domain/User.java
@@ -11,6 +11,7 @@ import javax.persistence.*;
 @Entity
 @Table(name = "users")
 public class User {
+
     private int id;
     private String username;
     private String password;
