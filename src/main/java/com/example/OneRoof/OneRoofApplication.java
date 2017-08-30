0b9b527a4978e3c5953a5ee300f35e569ae28353
Ethan Philipson <erphilipson@gmail.com>
2017-08-30 11:39:16 -0400

    New CORS config, round 6

--- src/main/java/com/example/OneRoof/OneRoofApplication.java
+++ src/main/java/com/example/OneRoof/OneRoofApplication.java
@@ -22,7 +22,7 @@ public class OneRoofApplication {
 		return new WebMvcConfigurerAdapter() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/api/**").allowedOrigins("**");
+				registry.addMapping("/api/**").allowedOrigins("*");
 			}
 		};
 	}
