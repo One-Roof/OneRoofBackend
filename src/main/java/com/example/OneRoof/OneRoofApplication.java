7ce8f488fa3f531859d1724489642230755bcc56
Ethan Philipson <erphilipson@gmail.com>
2017-08-30 11:17:40 -0400

    Enable CORS

--- src/main/java/com/example/OneRoof/OneRoofApplication.java
+++ src/main/java/com/example/OneRoof/OneRoofApplication.java
@@ -2,7 +2,11 @@ package com.example.OneRoof;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableOAuth2Client
@@ -10,5 +14,16 @@ public class OneRoofApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OneRoofApplication.class, args);
+	}
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/*");
+			}
+		};
 	}
 }
--- src/main/java/com/example/OneRoof/config/WebSecurityConfig.java
+++ src/main/java/com/example/OneRoof/config/WebSecurityConfig.java
@@ -27,8 +27,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable();
+        http
                 .authorizeRequests()
-                    .antMatchers("/", "/newuser", "/signup", "/api/*").permitAll()
+                    .antMatchers("/", "/newuser", "/signup", "/api", "/api/*").permitAll()
                     .antMatchers("/admin").hasRole("ADMIN")
                     .anyRequest().authenticated()
                     .and()
