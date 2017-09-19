12afa5249d2e4bc3db9eb160801656c67312f4f1
Ethan Philipson <erphilipson@gmail.com>
2017-09-19 10:42:09 -0400

    Change CORS settings

--- src/main/java/com/example/OneRoof/config/BasicAuthConfig.java
+++ src/main/java/com/example/OneRoof/config/BasicAuthConfig.java
@@ -76,7 +76,7 @@ public class BasicAuthConfig extends WebSecurityConfigurerAdapter{
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
+        configuration.setAllowedOrigins(Arrays.asList("/**"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
