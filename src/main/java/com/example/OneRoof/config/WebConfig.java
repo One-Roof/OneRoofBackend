50385fff444abe16a6dc596a8bc1afd87a80c156
Ethan Philipson <erphilipson@gmail.com>
2017-09-28 11:15:56 -0400

    Add allowed headers to webconfig

--- src/main/java/com/example/OneRoof/config/WebConfig.java
+++ src/main/java/com/example/OneRoof/config/WebConfig.java
@@ -7,10 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * Created by Ethan on 9/19/17.
  */
 public class WebConfig extends WebMvcConfigurerAdapter {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+                .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept");
     }
 
 }
