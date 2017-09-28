ed12c0496c5defa013ac0c2e8f0a19066324c182
Ethan Philipson <erphilipson@gmail.com>
2017-09-28 14:08:26 -0400

    Add username JSON

--- src/main/java/com/example/OneRoof/controller/LoginController.java
+++ src/main/java/com/example/OneRoof/controller/LoginController.java
@@ -1,22 +1,25 @@
 package com.example.OneRoof.controller;
 
+import com.example.OneRoof.domain.User;
+import org.aspectj.weaver.ast.Test;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("/something")
-    @ResponseBody
-    public String printUser(ModelMap model) {
+    @PostMapping("/something")
+    public @ResponseBody
+    User printUser(User user) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String name = auth.getName();
-
-        model.addAttribute("username", name);
-        return name;
+        user.setName(name);
+        return user;
     }
 }
