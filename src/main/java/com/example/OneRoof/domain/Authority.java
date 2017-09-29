8264cf41e2144eda7cc543ecc85f28bcecd3a24d
Ethan Philipson <erphilipson@gmail.com>
2017-09-29 14:36:43 -0400

    Add id to authority

--- src/main/java/com/example/OneRoof/domain/Authority.java
+++ src/main/java/com/example/OneRoof/domain/Authority.java
@@ -3,6 +3,7 @@ package com.example.OneRoof.domain;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Data
@@ -10,6 +11,7 @@ import javax.persistence.Table;
 @Table(name = "authority")
 public class Authority {
 
+    @Id
     private String username;
     private String authority;
 
